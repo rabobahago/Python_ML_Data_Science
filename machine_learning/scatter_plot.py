2f0eca22e70b96d191b4ddd6dd6648cc07753c83
rabobahago <rabobahago@gmail.com>
2023-04-01 16:09:23 +0100

    regression ml solution added

--- machine_learning/scatter_plot.py
+++ machine_learning/scatter_plot.py
@@ -9,11 +9,16 @@
 # The y array represents the speed of each car.
 
 #Use the scatter() method to draw a scatter plot diagram:
-
+import numpy
 import matplotlib.pyplot as plt
 
 x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
 y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 plt.scatter(x, y)
+plt.show()
+
+x_axis = numpy.random.normal(0.0, 5.0, 100000)
+y_axis = numpy.random.normal(0.0, 5.0, 100000)
+plt.scatter(x_axis, y_axis)
 plt.show()
