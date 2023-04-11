8815528d24faeb2396638f0d5971f9bbe4ea5bd9
rabobahago <rabobahago@gmail.com>
2023-04-11 11:11:53 +0100

    random permutation added

---
+++ numpy/random_permu.py
@@ -0,0 +1,22 @@
+# Random Permutations of Elements
+# A permutation refers to an arrangement of elements. e.g. [3, 2, 1] is a permutation of [1, 2, 3] and vice-versa.
+# The NumPy Random module provides two methods for this: shuffle() and permutation().
+# Shuffling Arrays
+# Shuffle means changing arrangement of elements in-place. i.e. in the array itself.
+# Randomly shuffle elements of following array:
+
+from numpy import random
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+random.shuffle(arr)
+print(arr)
+
+# Generating Permutation of Arrays
+# Generate a random permutation of elements of following array:
+
+from numpy import random
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(random.permutation(arr))
