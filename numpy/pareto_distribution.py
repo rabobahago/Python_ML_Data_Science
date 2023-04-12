235146b834cd8d1c0fc52f7c3896970a2dc00164
rabobahago <rabobahago@gmail.com>
2023-04-12 17:08:09 +0100

    numpy pareto_distribution implemented

---
+++ numpy/pareto_distribution.py
@@ -0,0 +1,19 @@
+# A distribution following Pareto's law i.e. 80-20 distribution (20% factors cause 80% outcome).
+# It has two parameter:
+# a - shape parameter.
+# size - The shape of the returned array.
+# Draw out a sample for pareto distribution with shape of 2 with size 2x3:
+
+from numpy import random
+x = random.pareto(a=2, size=(2, 3))
+print(x)
+
+#Visualization of Pareto Distribution
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.pareto(a=2, size=1000), kde=False)
+
+plt.show()
