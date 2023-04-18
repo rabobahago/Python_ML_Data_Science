7e990ff58e410a46cd2d4e2aa9ff479fda5f140b
rabobahago <rabobahago@gmail.com>
2023-04-18 08:56:26 +0100

    folders directory changed and projects folder created

---
+++ projects/honey_production/honey_production.py
@@ -0,0 +1,7 @@
+#import codecademylib3_seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+df = pd.read_csv("honeyproduction.csv")
+print(df.head())
