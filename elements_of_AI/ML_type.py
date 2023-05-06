3c120b4f9f4cd1416bdadad2639003b451bbaa12
rabobahago <rabobahago@gmail.com>
2023-05-06 20:13:52 +0100

    classifier neigbour note added

---
+++ elements_of_AI/ML_type.py
@@ -0,0 +1,35 @@
+# MNIST – What’s that?
+# Every machine learning student knows about the MNIST dataset. Fewer know what the acronym
+# stands for. In fact, we had to look it up to be able to tell you that the M stands for Modified,
+# and NIST stands for National Institute of Standards and Technology. Now you probably know something
+# that an average machine learning expert doesn’t!
+
+# Three types of machine learning
+# The roots of machine learning are in statistics which can also be thought of as the art of extracting
+# knowledge from data. Especially methods such as linear regression and Bayesian statistics, which are
+# both already more than two centuries old (!), are even today at the heart of machine learning.
+
+# Supervised learning: We are given an input, for example a photograph with a traffic sign, and the task
+# is to predict the correct output or label, for example which traffic sign is in the picture
+# (speed limit, stop sign, etc.). In the simplest cases, the answers are in the form of yes/no
+# (we call these binary classification problems).
+
+# Unsupervised learning: There are no labels or correct outputs. The task is to discover the
+# structure of the data: for example, grouping similar items to form “clusters”, or reducing
+# the data to a small number of important “dimensions”. Data visualization can also be considered unsupervised learning.
+
+# Reinforcement learning: Commonly used in situations where an AI agent like a self-driving car must operate in an
+# environment and where feedback about good or bad choices is available with some delay. Also used in games where the
+# outcome may be decided only at the end of the game.
+
+# The categories are somewhat overlapping and fuzzy, so a particular method can sometimes be hard to place in one
+# category. For example, as the name suggests, so-called semisupervised learning is partly supervised and partly unsupervised.
+
+#summary:
+#There are three type of ML
+#supervised learning: This is binary classification such as yes/no question, the type of of machine learning always
+#has required output or label.
+#unsupervised learning: This is type of machine learning doeasn't has required output or label. It involve grouping similar
+#items into a cluster of items. It can be data visualization
+#reinforcement Learning: Commonly used in situations where an AI agent like a self-driving car must operate in an environment
+# and where feedback about good or bad choices is available with some delay.
---
+++ elements_of_AI/classifier_neigbour.py
@@ -0,0 +1,8 @@
+# The nearest neighbor classifier
+# The nearest neighbor classifier is among the simplest possible classifiers.
+# When given an item to classify, it finds the training data item that is most
+# similar to the new item, and outputs its label. An example is given in the following diagram.
+
+#nearest classification:This uses input data to predict the label(text output)
+#linear regression: This uses the input data to predict the output in terms of numerical
+#logistic regression: This uses the input data to predict the output in terms of label representing numbers
