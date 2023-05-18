85c0108ad448fdf342c9d97fe7dc194e30bc61cc
rabobahago <rabobahago@gmail.com>
2023-05-18 20:24:53 +0100

    dependencies version test added

---
+++ ML_in_14days/day_one.py
@@ -0,0 +1,24 @@
+import sys
+
+#python version
+print('python: {}'.format(sys.version))
+
+#scipy version
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+#numpy version
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+#matplotlib version
+import matplotlib
+print('mattplotlib: {}'.format(matplotlib.__version__))
+
+# pandas version
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+
+# scikit-learn version
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
