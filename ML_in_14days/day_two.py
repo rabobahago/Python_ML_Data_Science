dc81520481d99fc7846c254e48f3e7ff7cc4360c
rabobahago <rabobahago@gmail.com>
2023-05-19 09:11:51 +0100

    Machine learning day two added

---
+++ ML_in_14days/day_two.py
@@ -0,0 +1,7 @@
+import numpy
+import pandas
+myarray = numpy.array([[1,2, 3], [4,5,6]])
+rownames = ['a', 'b']
+colnames = ['one', 'two', 'three']
+mydataframe = pandas.DataFrame(myarray, index=rownames, columns=colnames)
+print(mydataframe)
