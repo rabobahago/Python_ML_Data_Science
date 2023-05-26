c8ae1c422f6776943a21fd0f3894287a655bdb6a
rabobahago <rabobahago@gmail.com>
2023-05-26 21:38:06 +0100

    split dataset in train, validation, and test data added

--- magic_gamma_telescope/magic_data_set.py
+++ magic_gamma_telescope/magic_data_set.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 cols = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist', 'class']
 df = pd.read_csv("magic04.data", names=cols)
+print(df.shape)
 df['class'] = (df['class'] == 'g').astype(int)
-df.head()
+# df.head()
 for label in cols[:-1]:
     plt.hist(df[df['class']== 1][label], color='blue', label='gamma', alpha = 0.7, density = True)
     plt.hist(df[df['class']== 0][label], color='red', label='latron', alpha = 0.7, density = True)
@@ -13,3 +14,4 @@ for label in cols[:-1]:
     plt.xlabel(label)
     plt.legend()
     plt.show()
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
