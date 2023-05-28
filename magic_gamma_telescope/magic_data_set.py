606382e31df167fd4dc746055f8ba15df62f8bbb
rabobahago <rabobahago@gmail.com>
2023-05-28 10:06:12 +0100

    K_Near_Neighbors added

--- magic_gamma_telescope/magic_data_set.py
+++ magic_gamma_telescope/magic_data_set.py
@@ -1,32 +1,48 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
-cols = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist', 'class']
+cols = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1',
+        'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist', 'class']
 df = pd.read_csv("magic04.data", names=cols)
 print(df.shape)
 df['class'] = (df['class'] == 'g').astype(int)
 # df.head()
 for label in cols[:-1]:
-    plt.hist(df[df['class']== 1][label], color='blue', label='gamma', alpha = 0.7, density = True)
-    plt.hist(df[df['class']== 0][label], color='red', label='latron', alpha = 0.7, density = True)
+    plt.hist(df[df['class'] == 1][label], color='blue',
+             label='gamma', alpha=0.7, density=True)
+    plt.hist(df[df['class'] == 0][label], color='red',
+             label='latron', alpha=0.7, density=True)
     plt.title(label)
     plt.ylabel('Probability')
     plt.xlabel(label)
     plt.legend()
     plt.show()
-train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
-def scale_data(dataframe, oversample = False):
+train, valid, test = np.split(
+    df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+
+
+def scale_data(dataframe, oversample=False):
     X = dataframe[dataframe.columns[:-1]].values
     y = dataframe[dataframe.columns[-1]].values
-    scale =StandardScaler()
+    scale = StandardScaler()
     X = scale.fit_transform(X)
     if oversample:
         ros = RandomOverSampler()
         X, y = ros.fit_resample(X, y)
-    data = np.hstack((X, np.reshape(y,(-1, 1))))
+    data = np.hstack((X, np.reshape(y, (-1, 1))))
     return data, X, y
-train, X_train, x_train = scale_data(train, oversample=True)
-valid, X_valid, x_valid = scale_data(valid, oversample=False)
-test, X_test, x_test = scale_data(test, oversample=False)
+
+
+train, X_train, y_train = scale_data(train, oversample=True)
+valid, X_valid, y_valid = scale_data(valid, oversample=False)
+test, X_test, y_test = scale_data(test, oversample=False)
+knn_model = KNeighborsClassifier(n_neighbors=1)
+knn_model.fit(X_train, y_train)
+y_predict = knn_model.predict(X_test)
+print(y_predict)
+print(y_test)
+print(classification_report(y_test, y_predict))
