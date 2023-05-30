4531fa5027b643215ef45def79f908e68f544c09
rabobahago <rabobahago@gmail.com>
2023-05-30 14:40:41 +0100

    single linear regression completed

--- magic_gamma_telescope/bikes_regression.py
+++ magic_gamma_telescope/bikes_regression.py
@@ -23,3 +23,32 @@ for label in df.columns[1:]:
     plt.ylabel("Bike Count at Noon")
     plt.xlabel(label)
     plt.show()
+df = df.drop(["wind", "visibility", "functional"], axis=1)
+
+train, val, test = np.split(
+    df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+df.head()
+
+
+def get_xy(dataframe, y_label, x_labels=None):
+    dataframe = copy.deepcopy(dataframe)
+    if x_labels is None:
+        X = dataframe[[c for c in dataframe.columns if c != y_label]].values
+    else:
+        if len(x_labels) == 1:
+            X = dataframe[x_labels[0]].values.reshape(-1, 1)
+        else:
+            X = dataframe[x_labels].values
+
+    y = dataframe[y_label].values.reshape(-1, 1)
+    data = np.hstack((X, y))
+
+    return data, X, y
+
+
+_, X_train_temp, y_train_temp = get_xy(train, "bike_count", x_labels=["temp"])
+_, X_val_temp, y_val_temp = get_xy(val, "bike_count", x_labels=["temp"])
+_, X_test_temp, y_test_temp = get_xy(test, "bike_count", x_labels=["temp"])
+temp_reg = LinearRegression()
+temp_reg.fit(X_train_temp, y_train_temp)
+temp_reg.score(X_test_temp, y_test_temp)
