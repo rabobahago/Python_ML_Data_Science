bb79e77c52af942c7c9da1b7a8cc82f1586d5aee
rabobahago <rabobahago@gmail.com>
2023-06-02 23:05:01 +0100

    series computable columns added

--- series.py
+++ series.py
@@ -4,6 +4,10 @@ b = pd.Series(["I", "like", "to", "play", "games", "and",
               "will", "not", "change"], index=range(0, 9))
 c = pd.DataFrame([a, b])
 columns = pd.DataFrame({"A": a, "B": b})
-print(columns)
-transponse = pd.DataFrame([a, b]).T.rename(columns={0: "A", 1: "B"})
-print(transponse)
+df = pd.DataFrame([a, b]).T.rename(columns={0: "A", 1: "B"})
+another = df[['A', 'B']]
+print(another[(another["A"] > 5) & (another["B"] == 'and')])
+df["DivA"] = df["A"] - df["A"].mean()
+df['LenB'] = df["B"].apply(lambda x: len(x))
+df['Add'] = 2 + df['B'].apply(len)
+print(df)
