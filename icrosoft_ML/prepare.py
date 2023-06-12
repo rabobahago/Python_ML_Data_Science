0c94d366db2fb3478fe2c141dd9302734602aadc
rabobahago <rabobahago@gmail.com>
2023-06-12 20:52:49 +0100

    data preparation and visualization added

---
+++ icrosoft_ML/prepare.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# read from CSV File
+pumpkins = pd.read_csv('./pumpkins.csv')
+
+pumpkins = pumpkins[pumpkins['Package'].str.contains(
+    'bushel', case=True, regex=True)]
+# check for null and sum for column
+pumpkins.isnull().sum()
+# select specific columns
+columns_to_select = ['Package', 'Low Price', 'High Price', 'Date']
+# select all rows of the selected columns
+pumpkins = pumpkins.loc[:, columns_to_select]
+# Average price of pumpkins
+price = (pumpkins['Low Price'] + pumpkins['High Price']) / 2
+# Month
+month = pd.DatetimeIndex(pumpkins['Date']).month
+
+# copy your converted data into a fresh Pandas dataframe
+
+new_pumpkins = pd.DataFrame(
+    {'Month': month, 'Package': pumpkins['Package'], 'Low Price': pumpkins['Low Price'], 'High Price': pumpkins['High Price'], 'Price': price})
+new_pumpkins.loc[new_pumpkins['Package'].str.contains(
+    '1 1/9'), 'Price'] = price/(1 + 1/9)
+
+new_pumpkins.loc[new_pumpkins['Package'].str.contains(
+    '1/2'), 'Price'] = price/(1/2)
+price = new_pumpkins.Price
+month = new_pumpkins.Month
+plt.scatter(price, month)
+plt.show()
+
+new_pumpkins.groupby(['Month'])['Price'].mean().plot(kind='bar')
+plt.ylabel("Pumpkin Price")
