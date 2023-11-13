865cef937e3e749b5cf2b3a804219694378fa480
rabobahago <rabobahago@gmail.com>
2023-11-13 17:16:44 +0100

    binary conversion algorithm added

---
+++ algorithm/conversion_to_binary/binary.py
@@ -0,0 +1,54 @@
+value = int(input('Enter a number between 0-1023'))
+binary_string = ''
+if value >= 0 and value < 1024:
+    if value >= 512:
+        binary_string += '1'
+        value %= 512
+    else:
+        binary_string += '0'
+    if value >= 256:
+        binary_string += '1'
+        value %= 256
+    else:
+        binary_string += '0'
+    if value >= 128:
+        binary_string += '1'
+        value %= 128
+    else:
+        binary_string += '0'
+    if value >= 64:
+        binary_string += '1'
+        value %= 64
+    else:
+        binary_string += '0'
+    if value >= 32:
+        binary_string += '1'
+        value %= 32
+    else:
+        binary_string += '0'
+    if value >= 16:
+        binary_string += '1'
+        value %= 16
+    else:
+        binary_string += '0'
+    if value >= 8:
+        binary_string += '1'
+        value %= 8
+    else:
+        binary_string += '0'
+    if value >= 4:
+        binary_string += '1'
+        value %= 4
+    else:
+        binary_string += '0'
+    if value >= 2:
+        binary_string += '1'
+        value %= 2
+    else:
+        binary_string += '0'
+binary_string += str(value)
+# Display the results
+if binary_string != '':
+    print(binary_string)
+else:
+    print('Cannot convert')
