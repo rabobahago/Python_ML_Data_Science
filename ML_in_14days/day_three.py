ad0b279c5b58e1c221763f297059216aa153ea86
rabobahago <rabobahago@gmail.com>
2024-01-02 13:02:02 +0100

    data convert from row in 2d list implemented

--- ML_in_14days/day_three.py
+++
@@ -1,8 +0,0 @@
-# Load Data From CSV
-import pandas
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = pandas.read_csv(url, names=names)
-
-#(768 rows, 9 columns)
-print(data.shape)
